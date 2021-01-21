24af3f385a0c49f7bd75ae9079f6521cc06d760a
jasonruan <rzexin@qq.com>
2021-01-21 14:52:22 +0800

    【GoSDK】证书吊销及单元测试用例开发完成。

--- sdk_cert_manage_test.go
+++ sdk_cert_manage_test.go
@@ -43,6 +43,9 @@ func TestCertManage(t *testing.T) {
 		certs = []string{
 			"-----BEGIN CERTIFICATE-----\nMIICiDCCAi6gAwIBAgIDCuSTMAoGCCqGSM49BAMCMIGKMQswCQYDVQQGEwJDTjEQ\nMA4GA1UECBMHQmVpamluZzEQMA4GA1UEBxMHQmVpamluZzEfMB0GA1UEChMWd3gt\nb3JnMi5jaGFpbm1ha2VyLm9yZzESMBAGA1UECxMJcm9vdC1jZXJ0MSIwIAYDVQQD\nExljYS53eC1vcmcyLmNoYWlubWFrZXIub3JnMB4XDTIwMTExNjA2NDYwNFoXDTI1\nMTExNTA2NDYwNFowgZAxCzAJBgNVBAYTAkNOMRAwDgYDVQQIEwdCZWlqaW5nMRAw\nDgYDVQQHEwdCZWlqaW5nMR8wHQYDVQQKExZ3eC1vcmcyLmNoYWlubWFrZXIub3Jn\nMQ8wDQYDVQQLEwZjbGllbnQxKzApBgNVBAMTImNsaWVudDEudGxzLnd4LW9yZzIu\nY2hhaW5tYWtlci5vcmcwWTATBgcqhkjOPQIBBggqhkjOPQMBBwNCAAQjsmPDqPjx\nikMpRPkmWH8RFgUXwpzwaoMF9OQY6sAty2U8Q6TPlafMbm/xBls//UPZpi5uhwTv\neunkar0HqfvRo3sweTAOBgNVHQ8BAf8EBAMCAaYwDwYDVR0lBAgwBgYEVR0lADAp\nBgNVHQ4EIgQgjqe9Y2WHp+WC/GfKlvwummg3xvKPi9hbDja0QVFKa/EwKwYDVR0j\nBCQwIoAgmZcrtYWpTzN56LDZdqiHah3fG5w0kPaLoEBtyC8GfaEwCgYIKoZIzj0E\nAwIDSAAwRQIgbz8Du0bvtlWVJfBFzUamyfY2OodQDGBbKnr/eFXNeIECIQDnnJs5\nAX2NCT42Be3et+jhwxshehNsYm3WOOdTq/y+yg==\n-----END CERTIFICATE-----\n",
 		}
+
+		// org2 client证书的CRL
+		certCrl = "-----BEGIN CRL-----\nMIIBXTCCAQMCAQEwCgYIKoZIzj0EAwIwgYoxCzAJBgNVBAYTAkNOMRAwDgYDVQQI\nEwdCZWlqaW5nMRAwDgYDVQQHEwdCZWlqaW5nMR8wHQYDVQQKExZ3eC1vcmcyLmNo\nYWlubWFrZXIub3JnMRIwEAYDVQQLEwlyb290LWNlcnQxIjAgBgNVBAMTGWNhLnd4\nLW9yZzIuY2hhaW5tYWtlci5vcmcXDTIxMDEyMTA2NDYwM1oXDTIxMDEyMTEwNDYw\nM1owFjAUAgMK5JMXDTI0MDMyMzE1MDMwNVqgLzAtMCsGA1UdIwQkMCKAIJmXK7WF\nqU8zeeiw2Xaoh2od3xucNJD2i6BAbcgvBn2hMAoGCCqGSM49BAMCA0gAMEUCIEgb\nQsHoMkKAKAurOUUfAJpb++DYyxXS3zhvSWPxIUPWAiEAyLSd4TgB9PbSgHyGzS5D\nU1knUTu/4HKTol6GuzmV0Kg=\n-----END CRL-----"
 	)
 
 	client1, err = createClientWithOrgId(orgId1)
@@ -54,12 +57,19 @@ func TestCertManage(t *testing.T) {
 	fmt.Println("====================== 证书冻结 ======================")
 	testCertManageFrozen(t, client1, admin1, certs)
 	fmt.Println("====================== 已冻结，不可用 ======================")
+	// authentication failed, checking certificate frozen list returns error: certificate is frozen
 	testCertIsAvailable(t, false)
 
 	fmt.Println("====================== 证书解冻 ======================")
 	testCertManageUnfrozen(t, client1, admin1, certs)
 	fmt.Println("====================== 已解冻，可用 ======================")
 	testCertIsAvailable(t, true)
+
+	fmt.Println("====================== 证书吊销 ======================")
+	testCertManageRevoke(t, client1, admin1, certCrl)
+	fmt.Println("====================== 已吊销，不可用 ======================")
+	// authentication failed, checking CRL returns error: certificate is revoked
+	testCertIsAvailable(t, false)
 }
 
 func testCertIsAvailable(t *testing.T, isAvailable bool) {
@@ -117,8 +127,27 @@ func testCertManageUnfrozen(t *testing.T, client1, admin1 *ChainClient, certs []
 	fmt.Printf("resp: %+v, result:%+s\n", resp, result)
 }
 
-func testCertManageRevoke(t *testing.T) {
+func testCertManageRevoke(t *testing.T, client1, admin1 *ChainClient, certCrl string) {
+	var (
+		err error
+		payload []byte
+		signedPayloadBytes []byte
+		resp *pb.TxResponse
+		result string
+	)
 
+	payload, err = client1.CreateCertManageRevocationPayload(certCrl)
+	require.Nil(t, err)
+
+	signedPayloadBytes, err = admin1.SignCertManagePayload(payload)
+	require.Nil(t, err)
+
+	resp, err = client1.SendCertManageRequest(signedPayloadBytes, -1, true)
+	require.Nil(t, err)
+
+	result = string(resp.ContractResult.Result)
+
+	fmt.Printf("resp: %+v, result:%+s\n", resp, result)
 }
 
 func testCertAdd(t *testing.T, client *ChainClient) string {
