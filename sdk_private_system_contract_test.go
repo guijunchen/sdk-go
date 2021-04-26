6ce821df913e57f5eaa3b4107c065886eb8d63a4
p_mosesding <893588380@qq.com>
2021-04-26 21:52:20 +0800

    modify test for private system contract sdk

--- sdk_private_system_contract_test.go
+++ sdk_private_system_contract_test.go
@@ -36,23 +36,11 @@ func TestChainClient_SaveCert(t *testing.T) {
 		{
 			name: "test1",
 			args: args{
-				userCert:       "",
-				enclaveCert:    "",
+				userCert:       "user1",
+				enclaveCert:    "enclave1",
 				txId:           "",
-				withSyncResult: true,
-				timeout:        1000,
-			},
-			want:    []byte{},
-			wantErr: true,
-		},
-		{
-			name: "test2",
-			args: args{
-				userCert:       "",
-				enclaveCert:    "",
-				txId:           "",
-				withSyncResult: true,
-				timeout:        1000,
+				withSyncResult: false,
+				timeout:        1,
 			},
 			want:    []byte{},
 			wantErr: true,
@@ -64,155 +52,67 @@ func TestChainClient_SaveCert(t *testing.T) {
 			cc, err := createClient()
 			require.Nil(t, err)
 			got, err := cc.SaveCert(tt.args.userCert, tt.args.enclaveCert, tt.args.txId, tt.args.withSyncResult, tt.args.timeout)
-			if (err != nil) != tt.wantErr {
-				t.Errorf("SaveCert() error = %v, wantErr %v", err, tt.wantErr)
+			if err != nil {
+				t.Errorf("SaveCert() error = %v, response %v", err, got)
 				return
 			}
-			if !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("SaveCert() got = %v, want %v", got, tt.want)
-			}
+
 		})
 	}
 }
 
-func TestChainClient_GetContract(t *testing.T) {
+func TestChainClient_SaveContract(t *testing.T) {
 
 	type args struct {
-		userCert     string
-		contractName string
-		codeHash     string
-		hashSign     string
+		contractCode   []byte
+		codeHash       string
+		contractName   string
+		txId           string
+		version        string
+		withSyncResult bool
+		timeout        int64
 	}
-
 	tests := []struct {
 		name    string
 		args    args
-		want    *common.PrivateGetContract
+		want    *common.ContractResult
 		wantErr bool
 	}{
 		{
 			name: "test1",
 			args: args{
-				userCert:     "1",
-				contractName: "2",
-				codeHash:     "1",
-				hashSign:     "1",
+				contractName:  "JUSTTEST2",
+				contractCode:   []byte("zhe ci yi ding hui cheng gong."),
+				codeHash:       "",
+				version:        version,
+				withSyncResult: false,
+				timeout:        1,
 			},
-
-			want:    nil,
-			wantErr: true,
-		},
-
-		{
-			name: "test2",
-			args: args{
-				userCert:     "",
-				contractName: "my_asset",
-				codeHash:     "",
-				hashSign:     "",
-			},
-
-			want: &common.PrivateGetContract{
-				ContractCode: nil,
-				GasLimit:     0,
+			want: &common.ContractResult{
+				Code:    0,
+				Result:  nil,
+				Message: "OK",
+				GasUsed: 0,
 			},
 			wantErr: true,
 		},
 	}
-
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			cc, err := createClient()
 			require.Nil(t, err)
-			got, err := cc.GetContract(tt.args.userCert, tt.args.contractName, tt.args.codeHash, tt.args.hashSign)
-			if (err != nil) != tt.wantErr {
-				t.Errorf("GetContract() error = %v, wantErr %v", err, tt.wantErr)
+			got, err := cc.SaveContract(tt.args.contractCode, tt.args.codeHash, tt.args.contractName, tt.args.txId,
+				tt.args.version, tt.args.withSyncResult, tt.args.timeout)
+			if err != nil{
+				t.Errorf("SaveContract() error = %v, wantErr %v", err, tt.wantErr)
 				return
 			}
-			if !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("GetContract() got = %v, want %v", got, tt.want)
+			if got.Message != "OK"{
+				t.Errorf("SaveContract() got = %v, want %v", got, tt.want)
 			}
 		})
 	}
 }
-
-func TestChainClient_GetData(t *testing.T) {
-
-	type args struct {
-		contractName string
-		privateKey   string
-		userCert     string
-		dirSign      string
-	}
-
-	tests := []struct {
-		name    string
-		args    args
-		want    []byte
-		wantErr bool
-	}{
-		//{
-		//	name: "test1",
-		//	args: args{
-		//		contractName: computeContract,
-		//		privateKey:   "key1",
-		//		userCert:     "",
-		//		dirSign:      "",
-		//	},
-		//	want:    []byte{},
-		//	wantErr: true,
-		//},
-		//{
-		//	name: "test2",
-		//	args: args{
-		//		contractName: computeContract,
-		//		privateKey:   "key3",
-		//		userCert:     "",
-		//		dirSign:      "",
-		//	},
-		//	want:    []byte{},
-		//	wantErr: true,
-		//},
-		{
-			name: "test3",
-			args: args{
-				contractName: computeContract,
-				privateKey:   "key5",
-				userCert:     "",
-				dirSign:      "",
-			},
-			want:    []byte("value_5"),
-			wantErr: true,
-		},
-		//{
-		//	name: "test4",
-		//	args: args{
-		//		contractName: computeContract,
-		//		privateKey:   "dir_key1",
-		//		userCert:     "",
-		//		dirSign:      "",
-		//	},
-		//	want:    []byte{},
-		//	wantErr: true,
-		//},
-	}
-
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			cc, err := createClient()
-			require.Nil(t, err)
-			got, err := cc.GetData(tt.args.contractName, tt.args.privateKey, tt.args.userCert, tt.args.dirSign)
-			if err != nil || tt.wantErr != true {
-				t.Errorf("SaveData() error = %v, wantErr %v", err, tt.wantErr)
-				return
-			}
-			if !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("GetData() got = %v, want %v", got, tt.want)
-			}
-		})
-	}
-}
-
 func TestChainClient_SaveData(t *testing.T) {
 
 	type args struct {
@@ -308,7 +208,7 @@ func TestChainClient_SaveDir(t *testing.T) {
 					StrArr: []string{"dir_key1", "dir_key2", "dir_key3"},
 				},
 				txId:           "",
-				withSyncResult: true,
+				withSyncResult: false,
 				timeout:        1,
 			},
 			want:    []byte{},
@@ -322,7 +222,7 @@ func TestChainClient_SaveDir(t *testing.T) {
 			require.Nil(t, err)
 			got, err := cc.SaveDir(tt.args.userCert, tt.args.orderId, tt.args.dirHash, tt.args.dirSign, tt.args.txId,
 				tt.args.privateDir, tt.args.withSyncResult, tt.args.timeout)
-			if string(got) != "OK" || err != nil || tt.wantErr != true {
+			if string(got) != "OK" || err != nil{
 				t.Errorf("SaveDir() error = %v, wantErr %v", err, tt.wantErr)
 				return
 			}
@@ -330,54 +230,128 @@ func TestChainClient_SaveDir(t *testing.T) {
 	}
 }
 
-func TestChainClient_SaveContract(t *testing.T) {
+func TestChainClient_GetContract(t *testing.T) {
 
 	type args struct {
-		contractCode   []byte
-		codeHash       string
-		contractName   string
-		txId           string
-		version        string
-		withSyncResult bool
-		timeout        int64
+		userCert     string
+		contractName string
+		codeHash     string
+		hashSign     string
 	}
+
 	tests := []struct {
 		name    string
 		args    args
-		want    *common.ContractResult
-		wantErr bool
+		want    *common.PrivateGetContract
+		wantErr interface{}
 	}{
 		{
-			name: "test1",
+			name: "test2",
 			args: args{
-				contractCode:   nil,
-				codeHash:       "",
-				version:        version,
-				withSyncResult: false,
-				timeout:        1,
+				userCert:     "",
+				contractName: "JUSTTEST2",
+				codeHash:     "",
+				hashSign:     "",
 			},
-			want: &common.ContractResult{
-				Code:    0,
-				Result:  nil,
-				Message: "",
-				GasUsed: 0,
+			want: &common.PrivateGetContract{
+				ContractCode: []byte("zhe ci yi ding hui cheng gong."),
+				GasLimit:     10000000000,
 			},
-			wantErr: true,
+			wantErr: nil,
 		},
 	}
+
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			cc, err := createClient()
 			require.Nil(t, err)
-			got, err := cc.SaveContract(tt.args.contractCode, tt.args.codeHash, tt.args.contractName, tt.args.txId,
-				tt.args.version, tt.args.withSyncResult, tt.args.timeout)
-			if (err != nil) != tt.wantErr {
-				t.Errorf("SaveContract() error = %v, wantErr %v", err, tt.wantErr)
+			got, err := cc.GetContract(tt.args.userCert, tt.args.contractName, tt.args.codeHash, tt.args.hashSign)
+			code := string(got.ContractCode)
+			if err != nil{
+				t.Errorf("GetContract() error = %v, wantErr %v, code %s", err, tt.wantErr, code)
 				return
 			}
 			if !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("SaveContract() got = %v, want %v", got, tt.want)
+				t.Errorf("GetContract() got = %v, want %v", got, tt.want)
 			}
 		})
 	}
 }
+
+func TestChainClient_GetData(t *testing.T) {
+
+	type args struct {
+		contractName string
+		privateKey   string
+		userCert     string
+		dirSign      string
+	}
+
+	tests := []struct {
+		name    string
+		args    args
+		want    []byte
+		wantErr bool
+	}{
+		//{
+		//	name: "test1",
+		//	args: args{
+		//		contractName: computeContract,
+		//		privateKey:   "key1",
+		//		userCert:     "",
+		//		dirSign:      "",
+		//	},
+		//	want:    []byte{},
+		//	wantErr: true,
+		//},
+		//{
+		//	name: "test2",
+		//	args: args{
+		//		contractName: computeContract,
+		//		privateKey:   "key3",
+		//		userCert:     "",
+		//		dirSign:      "",
+		//	},
+		//	want:    []byte{},
+		//	wantErr: true,
+		//},
+		{
+			name: "test3",
+			args: args{
+				contractName: computeContract,
+				privateKey:   "key5",
+				userCert:     "",
+				dirSign:      "",
+			},
+			want:    []byte("value_5"),
+			wantErr: true,
+		},
+		//{
+		//	name: "test4",
+		//	args: args{
+		//		contractName: computeContract,
+		//		privateKey:   "dir_key1",
+		//		userCert:     "",
+		//		dirSign:      "",
+		//	},
+		//	want:    []byte{},
+		//	wantErr: true,
+		//},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			cc, err := createClient()
+			require.Nil(t, err)
+			got, err := cc.GetData(tt.args.contractName, tt.args.privateKey, tt.args.userCert, tt.args.dirSign)
+			if err != nil || tt.wantErr != true {
+				t.Errorf("SaveData() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("GetData() got = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
