ae6a354d492a78199137bd18cb1ec1c051d41675
jasonruan <rzexin@qq.com>
2021-07-16 09:26:34 +0800

    add

--- utils/convert.go
+++ utils/convert.go
@@ -12,8 +12,23 @@ func U64ToBytes(i uint64) []byte {
 }
 
 func BytesToU64(b []byte) (uint64, error) {
-	if len(b) < 8 {
+	if len(b) != 8 {
 		return 0, errors.New("invalid uint64 bytes")
 	}
+
 	return binary.LittleEndian.Uint64(b), nil
+}
+
+func I64ToBytes(i int64) []byte {
+	var b = make([]byte, 8)
+	binary.LittleEndian.PutUint64(b, uint64(i))
+	return b
+}
+
+func BytesToI64(b []byte) (int64, error) {
+	if len(b) != 8 {
+		return 0, errors.New("invalid int64 bytes")
+	}
+
+	return int64(binary.LittleEndian.Uint64(b)), nil
 }
