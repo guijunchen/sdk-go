e7c1c9c862698793ed1e3d80506015ed6b4dde9d
via <overcc@icloud.com>
2021-07-27 21:31:08 +0800

    code update

--- utils/keys.go
+++ utils/keys.go
@@ -36,32 +36,26 @@ const (
 	KeyCertCrl    = "cert_crl"
 
 	// PrivateCompute keys
-	KeyOrderId    = "order_id"
-	KeyPrivateDir = "private_dir"
-
+	KeyOrderId      = "order_id"
+	KeyPrivateDir   = "private_dir"
 	KeyContractName = "contract_name"
 	KeyCodeHash     = "code_hash"
-
-	KeyResult     = "result"
-	KeyCodeHeader = "code_header"
-	KeyVersion    = "version"
-	KeyIsDeploy   = "is_deploy"
-	KeyRWSet      = "rw_set"
-	KeyEvents     = "events"
-	KeyReportHash = "report_hash"
-	KeySign       = "sign"
-	KeyKey        = "key"
-
-	KeyPayload   = "payload"
-	KeyOrgIds    = "org_ids"
-	KeySignPairs = "sign_pairs"
-
-	KeyCaCert = "ca_cert"
-
-	KeyEnclaveId = "enclave_id"
-	KeyReport    = "report"
-
-	KeyProof      = "proof"
-	KeyDeployReq  = "deploy_req"
-	KeyPrivateReq = "private_req"
+	KeyResult       = "result"
+	KeyCodeHeader   = "code_header"
+	KeyVersion      = "version"
+	KeyIsDeploy     = "is_deploy"
+	KeyRWSet        = "rw_set"
+	KeyEvents       = "events"
+	KeyReportHash   = "report_hash"
+	KeySign         = "sign"
+	KeyKey          = "key"
+	KeyPayload      = "payload"
+	KeyOrgIds       = "org_ids"
+	KeySignPairs    = "sign_pairs"
+	KeyCaCert       = "ca_cert"
+	KeyEnclaveId    = "enclave_id"
+	KeyReport       = "report"
+	KeyProof        = "proof"
+	KeyDeployReq    = "deploy_req"
+	KeyPrivateReq   = "private_req"
 )
