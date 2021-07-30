fd7981f3944c96f86de922b63cf0f194d4285295
wanghongda <harryhdwang@tencent.com>
2021-07-30 14:40:27 +0800

    add more ut

--- sdk_block_chain_test.go
+++ sdk_block_chain_test.go
@@ -6,35 +6,35 @@ SPDX-License-Identifier: Apache-2.0
 
 package chainmaker_sdk_go
 
-//import (
-//	"testing"
-//
-//	"github.com/stretchr/testify/require"
-//
-//	"chainmaker.org/chainmaker/pb-go/common"
-//)
-//
-//func TestCheckNewBlockChainConfig(t *testing.T) {
-//	tests := []struct {
-//		name string
-//		res  *common.TxResponse
-//		err  error
-//	}{
-//		{
-//			"valid request",
-//			&common.TxResponse{Code: common.TxStatusCode_SUCCESS},
-//			nil,
-//		},
-//	}
-//
-//	for _, tt := range tests {
-//		t.Run(tt.name, func(t *testing.T) {
-//			cli, err := newMockChainClient(WithConfPath(sdkConfigPathForUT))
-//			require.Nil(t, err)
-//			defer cli.Stop()
-//
-//			err = cli.CheckNewBlockChainConfig()
-//			require.Nil(t, err)
-//		})
-//	}
-//}
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+
+	"chainmaker.org/chainmaker/pb-go/common"
+)
+
+func TestCheckNewBlockChainConfig(t *testing.T) {
+	tests := []struct {
+		name string
+		res  *common.TxResponse
+		err  error
+	}{
+		{
+			"valid request",
+			&common.TxResponse{Code: common.TxStatusCode_SUCCESS},
+			nil,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			cli, err := newMockChainClient(WithConfPath(sdkConfigPathForUT))
+			require.Nil(t, err)
+			defer cli.Stop()
+
+			err = cli.CheckNewBlockChainConfig()
+			require.Nil(t, err)
+		})
+	}
+}
