228973f4f484ba3eb7d9bd828ba1dda60f8c2901
wanghongda <harryhdwang@tencent.com>
2021-08-19 16:51:40 +0800

    set OrgId = "" when cert is a thirdparty CA

--- sdk_utils.go
+++ sdk_utils.go
@@ -36,8 +36,13 @@ func SignPayload(keyBytes, crtBytes []byte, payload *common.Payload) (*common.En
 		return nil, fmt.Errorf("SignPayload failed, %s", err)
 	}
 
+	var orgId string
+	if len(crt.Subject.Organization) != 0 {
+		orgId = crt.Subject.Organization[0]
+	}
+
 	sender := &accesscontrol.Member{
-		OrgId:      crt.Subject.Organization[0],
+		OrgId:      orgId,
 		MemberInfo: crtBytes,
 		MemberType: accesscontrol.MemberType_CERT,
 	}
