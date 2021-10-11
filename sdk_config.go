817c039ebe39de45daf85b706d6d0022a8baa886
wanghongda <harryhdwang@tencent.com>
2021-10-11 16:23:08 +0800

    if pkcs11 is not set in the configuration file, ignore check

--- sdk_config.go
+++ sdk_config.go
@@ -557,7 +557,7 @@ func checkArchiveConfig(config *ChainClientConfig) error {
 }
 
 func checkPkcs11Config(config *ChainClientConfig) error {
-	if !config.pkcs11Config.Enabled {
+	if config.pkcs11Config == nil || !config.pkcs11Config.Enabled {
 		return nil
 	}
 	// 如果config.pkcs11Config.Enabled == true 则其他参数不能为空
